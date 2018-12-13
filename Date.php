f72528ff460d4f7fbf951256cf9c11eef2e0c1cf
PHP Grupa 1 <phpgrupa1@Admins-iMac-3.local>
2018-12-13 20:39:18 +0200

    date

---
+++ Date.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link href="Date.css" rel="stylesheet" type="text/css">
+</head>
+	
+<?php	
+date_default_timezone_set("Europe/Bucharest");
+	
+//	for($i=0; $i<10; $i++) {
+//		echo date("Y-m-d h:i:s") . "<br/>";
+//}
+//	for($i=0; $i<=100; $i++) {
+//		echo $i . "<br/>";
+//	}
+//	for($j=1; $j<=10; $j++) {
+//		echo"<div>";
+//		for($i=1; $i<=10; $i++) {
+//			echo "$j x $i = " . $j * $i. "<br/>";
+//		}
+//		echo"</div>";
+//	}
+?>
+
+<body>
+	<div id="Produs"> 
+	<?
+		for($j=1; $j<=10; $j++) {
+		for($i=1; $i<=10; $i++) {
+			echo "$j x $i = " . $j * $i. "<br/>";
+		}
+	}
+	?>
+	</div>
+</body>
+</html>
