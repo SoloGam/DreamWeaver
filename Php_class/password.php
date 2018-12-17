4d8b60a223f73e78eeb78d73657d3c173fe2ea00
PHP Grupa 1 <phpgrupa1@Admins-iMac-3.local>
2018-12-17 20:08:12 +0200

    111

--- Php_class/password.php
+++ Php_class/password.php
@@ -7,19 +7,43 @@
 
 <body>
 	<?
-	$pass = '';
+	$pass=' ';
 	if (isset($_POST['pass'])) $pass = $_POST['pass'];
 	?>
 	
 	<form method="post">
-		Parola <input type="text" placeholder="Type your password" name="pass" value="<?=$pass?>"><br>
+		Parola  <input type="text" value="<?=$pass?>" placeholder="Type your password" name="pass" ><br>
 				<input type="submit" value="Trimite">
 	</form>
 	
 	
 	
 	<?
-	
-	?>
+		/*
+			daca contine anumite grupe de caractere: 
+					litere mici; 
+					litere mari; 
+					caractere speciale !@#$%ˆ&*()_+=<>?,./;
+					cifre 
+					lungime
+		*/
+		
+		$score = 0;
+	    $found = 0;
+					  
+		if (strtolower($pass) != $pass) $score += 20; ;
+		
+	    $search = '!@#$%ˆ&*()_+=<±§>?,.\|/' ;
+					   
+			   for ($i = 0; $i < strlen($search); $i++) {
+				   if (strpos($pass, $search[$i])) $found ++;
+				   if ($found > 1) break;
+			   }
+		  
+	   $score += $found * 10;
+		
+	   echo $score;
+					  
+	  ?>
 </body>
 </html>
