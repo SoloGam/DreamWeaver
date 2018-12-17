4d4f4715f066e234c7db3357c578d70c31c74c30
PHP Grupa 1 <phpgrupa1@Admins-iMac-3.local>
2018-12-17 20:55:13 +0200

    123

--- Php_class/password.php
+++ Php_class/password.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Pass</title>
+<link href="stil.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
@@ -29,21 +30,46 @@
 		*/
 		
 		$score = 0;
-	    $found = 0;
-					  
-		if (strtolower($pass) != $pass) $score += 20; ;
-		
-	    $search = '!@#$%ˆ&*()_+=<±§>?,.\|/' ;
+	    
+	    $search = 0;
 					   
+		   function findstring ($pass, $score, $search){
+				$found = 0;	   
 			   for ($i = 0; $i < strlen($search); $i++) {
 				   if (strpos($pass, $search[$i])) $found ++;
 				   if ($found > 1) break;
 			   }
 		  
-	   $score += $found * 10;
+	     return $score + $found * 10;
+		   }
+					  
+		if ((strtolower($pass) != $pass) && (strtoupper($pass) != $pass)) $score += 20; 
+		
+	   $score = findstring($pass, $score, '1234567890');
+	   $score = findstring($pass, $score, '!@#$%ˆ&*()_+=<±§>?,.\|/');
+					   
+	   	
+   	   if (strlen($pass) > 4) $score += 10;
+   	   if (strlen($pass) > 6) $score += 10;
+   	   if (strlen($pass) > 8) $score += 10;
+   	   if (strlen($pass) > 12) $score += 10;
+		
+	    
 		
 	   echo $score;
+		   $red = round(255*(100 - $score) / 100);
+		   $green= round(255*($score - 100) / 100);
+		   $color = 'rgb('.$red.','.$green.',0)';
 					  
 	  ?>
+	
+<!--		<meter min="0" max="100" value="<?=$score?>"></meter>-->
+	
+	<div class="Container">
+		<div id="bar" style= "width:<?=$score*2?>px; background-color: <?=$color?>; height: 50px;"></div>
+	
+	</div>
+	
+	
 </body>
 </html>
