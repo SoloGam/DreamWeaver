b4b50156b0ea63dbbf058c9ac6457342ccb22bce
PHP Grupa 1 <phpgrupa1@Admins-iMac-3.local>
2019-01-14 20:36:59 +0200

    gdysd

--- Php_class/Go.php
+++ Php_class/Go.php
@@ -7,28 +7,9 @@
 <meta charset="UTF-8">
 <title>Fuel/Distance</title>
 </head>
-
-	<?php
-	session_start();
-	if (!isset($_SESSION['car'])){
-		$car = new Car();
-
-$car -> total_fuel = 50;//L
-$car -> crt_fuel = 25;//L
-$car -> consumation = 10;// L/100km 
-		$_SESSION['car'] = $car;
-	}else{
-		$car = $_SESSION['car'];	
-	}
-	if (isset($_POST['distance'])) {
-		
-		echo $car -> go ($_POST['distance']);
-		echo "Benzina Ramasa: {$car -> fuel_left()}%";
-		$_SESSION['car'] = $car;
-	}
-	?>
-	
-	
+<?php
+require 'class_car_2.php';	
+?>
 	<form method="post" >
 	Cat vrei sa mergi?
 		<input type="text" name="distance">
